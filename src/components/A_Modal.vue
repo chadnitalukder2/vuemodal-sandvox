9c8a97ddfd1f5875362050ee9d22d5daccb6ab32
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-15 19:25:31 +0600

    2nd

--- src/components/A_Modal.vue
+++ src/components/A_Modal.vue
@@ -1,28 +1,29 @@
 <template>
   <transition name="modal-fade">
     <div class="modal-backdrop">
-        <div class="modal">
-            <h1>Hello! I'm a Modal</h1>
-            <footer class="modal-footer">
-                <slot  name="footer">
-                    <button @click="close" type="button" class="btn-green">Close me</button>
-                </slot>
-            </footer>
-        </div>
+      <div class="modal">
+        <h1>Hello! I'm a Modal</h1>
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button @click="close" type="button" class="btn-green">
+              Close me
+            </button>
+          </slot>
+        </footer>
+      </div>
     </div>
-
   </transition>
 </template>
 
 <script>
-    export default {
-        name: "A_Modal",
-        methods:{
-            close(){
-                this.$emit("close")
-            }
-        }
-    }
+export default {
+  name: "A_Modal",
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
 </script>
 
 <style>
@@ -34,35 +35,35 @@
 .modal-fade-leave-active {
   transition: opacity 0.5s ease;
 }
-.modal-backdrop{
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color:  rgba(0, 0, 0, 0.3) ;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.modal{
-    background: #fff ;
-    box-shadow: 2px 2px 20px 1px;
-    overflow-x: auto;
-    display: flex;
-    flex-direction: column;
+.modal {
+  background: #fff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
 }
 .modal-header,
-.modal-footer{
-    padding: 5px;
-    text-align: center;
+.modal-footer {
+  padding: 5px;
+  text-align: center;
 }
-.modal-footer{
-    border-top: 1px solid #eee;
-    justify-content: flex-end;
+.modal-footer {
+  border-top: 1px solid #eee;
+  justify-content: flex-end;
 }
-.btn-close{
-    border: none;
+.btn-close {
+  border: none;
   font-size: 20px;
   padding: 20px;
   cursor: pointer;
@@ -71,15 +72,15 @@
   background: red;
   text-align: right;
 }
-.btn-green{
-    color: white;
-    background: #828282;
+.btn-green {
+  color: white;
+  background: #828282;
   border: 1px solid #828282;
   border-radius: 2px;
   margin: 5px;
 }
-.input{
-margin-top: 5px;
+.input {
+  margin-top: 5px;
 }
 .added-text {
   margin-block-end: -10px;
