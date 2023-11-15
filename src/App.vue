5e0a0581554ddaf33bfac8accd590484f918f58f
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-15 19:24:48 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <img  src="./assets/logo.png">
+    <h1>Hello, I'm a Vue Project</h1>
+    <button @click="showModal" class="btn">Click me!</button>
+    <Modal v-show="isModalVisible"  @close="closeModl"/>
+  </div>
+ 
+  
+</template>
+
+<script>
+import Modal from './components/A_Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data(){
+    return{
+      isModalVisible: false
+    };
+  },
+  methods:{
+    showModal(){
+      this.isModalVisible = true;
+    },
+    closeModl(){
+      this.isModalVisible = false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.btn{
+  color: white;
+  background: #000;
+  border: 1px solid #828282;
+  border-radius: 2px;
+  margin: 4px;
+  padding: 8px;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/A_Modal.vue
@@ -0,0 +1,88 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+        <div class="modal">
+            <h1>Hello! I'm a Modal</h1>
+            <footer class="modal-footer">
+                <slot  name="footer">
+                    <button @click="close" type="button" class="btn-green">Close me</button>
+                </slot>
+            </footer>
+        </div>
+    </div>
+
+  </transition>
+</template>
+
+<script>
+    export default {
+        name: "A_Modal",
+        methods:{
+            close(){
+                this.$emit("close")
+            }
+        }
+    }
+</script>
+
+<style>
+.modal-fade-enter,
+.modal-fade-leave-active {
+  opacity: 0;
+}
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.modal-backdrop{
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color:  rgba(0, 0, 0, 0.3) ;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.modal{
+    background: #fff ;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+}
+.modal-header,
+.modal-footer{
+    padding: 5px;
+    text-align: center;
+}
+.modal-footer{
+    border-top: 1px solid #eee;
+    justify-content: flex-end;
+}
+.btn-close{
+    border: none;
+  font-size: 20px;
+  padding: 20px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #000;
+  background: red;
+  text-align: right;
+}
+.btn-green{
+    color: white;
+    background: #828282;
+  border: 1px solid #828282;
+  border-radius: 2px;
+  margin: 5px;
+}
+.input{
+margin-top: 5px;
+}
+.added-text {
+  margin-block-end: -10px;
+  margin-block-start: 0.5rem;
+}
+</style>
